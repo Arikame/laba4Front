98f2809741ef4f751679e6f55d2daa315bed1fb1
Arikame <sandra2902@mail.ru>
2020-03-25 18:49:19 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Header extends React.Component {
+    render() {
+        return (
+
+                <div className="hat">
+                    <h1> Лабораторная работа #4</h1>
+                    <p>Выполнила: Лескова Александра Костантиновна</p>
+                    <p>Группа: Р3213</p>
+                    <p>Вариант: 524839</p>
+                </div>
+
+        );
+    }
+}
